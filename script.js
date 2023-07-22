5723aeaf1c204973d2cf381f668d4b351e62fc60
Montaz Meah <montaz.meah@mrm.com>
2023-07-22 18:18:25 -0400

    end of lesson

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ for (let i = 0; i < 3; i++) {
 
 
 // conditions
+if (true) {
+    console.log("yessssssss");
+}
+
+if (false) {
+    console.log("noooooooo");
+    console.log("other stuff");
+    let myName = "taz";
+}
+
+let myAge = 23;
+
+if (myAge > 21) {
+    console.log("give them beer");
+}
+
+
+let myHealth = 10;
+
+if (myHealth === 0) {
+    console.log("you died.");
+}
+
+if (myHealth !== 100) {
+    console.log("you're not at full health.");
+}
+
+if (myHealth < 11 && myHealth !== 0) {
+    console.log("you are near death.");
+}
